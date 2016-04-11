0ecc47ac79767413d64bb3eb046943003dab52ee
wzulfikar <wzulfikar@ymail.com>
2016-04-11 13:44:51 +0800

    added route to handle debit & credit

--- routes.php
+++ routes.php
@@ -13,3 +13,14 @@ Route::get('ledger/{account_id}', function($account_id){
 	$last_record = $account->ledger()->latest()->first();
 	return view('eloquent-simple-ledger.index', compact('account_id', 'last_record'));
 });
+
+Route::post('ledger/{account_id}', function($account_id){
+
+	$account = Wzulfikar\EloquentSimpleLedger\Account::findOrFail($account_id);
+
+	$amount = \Request::get('amount');
+	$desc   = \Request::get('desc');
+	$action = \Request::get('action'); // debit or credit
+
+	return ['success'=>$account->$action($amount, $desc), 'msg'=>'debit success'];
+});
